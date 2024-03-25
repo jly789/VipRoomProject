5f28a26863ba495b7e9ec023d5613588d73f966a
jly78 <jly789@naver.com>
2024-03-25 12:22:44 +0900

    VipRoomProject Create
    초기

---
+++ com.accommodation/src/test/java/vip/com/accommodation/ApplicationTests.java
@@ -0,0 +1,13 @@
+package vip.com.accommodation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
